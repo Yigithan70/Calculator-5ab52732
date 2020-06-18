31cb50ef858309b6d03d457cbe8b13fa5dd7c652
Yigithan Dincer <2084054@talnet.nl>
2020-06-18 13:35:34 +0200

    Uitwerking van de exercise

--- calculator.php
+++ calculator.php
@@ -1,29 +1,26 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+<meta charset="UTF-8">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<title>Document</title>
 </head>
 <body>
-
-    <?php 
-
-    $line1 = readline("Welke operatie wil je uitvoeren? (+,-)");
-        
-     	
-    if ($line1 == "+") {
-        $line = readline("eerste getal?");
-        $line = readline("tweede getal?");
-        echo "Het antwoord = "; 
-        $line = readline($line + $line);
-    } elseif ($line1 == "-") { 
-         $line = readline("eerste getal?");
-         $line = readline("tweede getal?");
-        echo "Het antwoord = "; 
-         $line = readline($line - $line);
-    }
-    ?>
+<?php 
+$line1 = readline("Welke operatie wil je uitvoeren? (+,-)");
+	
+if ($line1 == "+") { 
+	$line = readline("eerste getal?");
+$line = readline("tweede getal?");
+	echo "Het antwoord = "; 
+	$line = readline($line + $line);
+} elseif ($line1 == "-") { 
+		$line = readline("eerste getal?");
+		$line = readline("tweede getal?");
+	echo "Het antwoord = "; 
+		$line = readline($line - $line);
+}
+?>
 
 
 
