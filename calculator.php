7a9ee9041e93dac52383f29b979ea0a558d33bb1
Yigithan70 <60740198+Yigithan70@users.noreply.github.com>
2020-06-18 12:21:00 +0200

    Add files via upload

--- calculator.php
+++ calculator.php
@@ -1,12 +1,16 @@
 <?php
-$line1 = readline("Welke operatie wil je uitvoeren? (+,-)");
-$line2 = readline("Eerste getal? ");
-$line3 = readline("Tweede getal? ");
-$line4 = readline("Uw resultaat is: ");
-readline_add_history($line);
-if ($line1== "+") {
-$uitkomst=readline($line2+$line3); }
-elseif ($line1=="-") {
-$uitkomst=readline($line2-$line3);
-}
+
+	$line1 = readline("Welke operatie wil je uitvoeren? (+,-)");
+	$line2 = readline("Eerste getal?");
+	$line3 = readline("Tweede getal?");
+	$line4 = readline("Uw resultaat is:");
+	readline_add_history($line);
+
+	if ($line1 == "+"){
+		$uitkomst = readline($line2 + $line3); }
+
+	elseif ($line1 == "-"){
+		$uitkomst = readline($line2 - $line3);
+	}
+
 ?>
